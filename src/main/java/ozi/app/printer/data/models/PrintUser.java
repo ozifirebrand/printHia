7c7901f2025820474f27c261e781ff80f578f0b3
Ozioma <oziomaokoroafor@gmail.com>
2022-03-20 19:45:23 +0100

    Add user entity

---
+++ src/main/java/ozi/app/printer/data/models/PrintUser.java
@@ -0,0 +1,27 @@
+package ozi.app.printer.data.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+
+@Entity
+@Data
+public class PrintUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany
+    private List<PrintOrder> orders;
+}
