06f3c1f3b984b7f8248d0785c3f9f11ccf089fd1
Ozioma <oziomaokoroafor@gmail.com>
2022-03-20 19:55:09 +0100

    Add order attributes

--- src/main/java/ozi/app/printer/data/models/PrintOrder.java
+++ src/main/java/ozi/app/printer/data/models/PrintOrder.java
@@ -2,8 +2,22 @@ package ozi.app.printer.data.models;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
+
 @Entity
 public class PrintOrder {
     @Id
     private String id;
+
+    private String imageUrl;
+
+    private double size;
+
+    private int quantity;
+
+    private LocalDateTime deliveryDate;
+
+    private boolean hasOrdered;
+
+    private OrderStatus orderStatus;
 }
