c0c87f6eb2c61c06a82beb5024b202a020d730d8
Ozioma <oziomaokoroafor@gmail.com>
2022-03-20 20:24:43 +0100

    Add order service impl

---
+++ src/main/java/ozi/app/printer/services/orderService/OrderServices.java
@@ -0,0 +1,10 @@
+package ozi.app.printer.services.orderService;
+
+import ozi.app.printer.data.dtos.requests.UserCreationRequest;
+import ozi.app.printer.data.dtos.responses.UserCreationResponse;
+import ozi.app.printer.data.models.PrintUser;
+
+import java.util.List;
+
+public interface OrderServices {
+}
---
+++ src/main/java/ozi/app/printer/services/orderService/OrderServicesImpl.java
@@ -0,0 +1,14 @@
+package ozi.app.printer.services.orderService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ozi.app.printer.data.dtos.requests.UserCreationRequest;
+import ozi.app.printer.data.dtos.responses.UserCreationResponse;
+import ozi.app.printer.data.models.PrintUser;
+import ozi.app.printer.data.repositories.UserRepository;
+
+import java.util.List;
+
+@Service
+public class OrderServicesImpl implements OrderServices {
+}
