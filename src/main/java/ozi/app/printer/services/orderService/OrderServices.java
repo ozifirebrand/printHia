bff1e7a5fc112598cc5d404783ffbc051102911f
Ozioma <oziomaokoroafor@gmail.com>
2022-03-20 20:43:55 +0100

    Add order service impl

--- src/main/java/ozi/app/printer/services/orderService/OrderServices.java
+++ src/main/java/ozi/app/printer/services/orderService/OrderServices.java
@@ -1,10 +1,22 @@
 package ozi.app.printer.services.orderService;
 
-import ozi.app.printer.data.dtos.requests.UserCreationRequest;
-import ozi.app.printer.data.dtos.responses.UserCreationResponse;
-import ozi.app.printer.data.models.PrintUser;
+import org.hibernate.criterion.Order;
+import ozi.app.printer.data.dtos.requests.OrderCreationRequest;
+import ozi.app.printer.data.dtos.responses.OrderCreationResponse;
+import ozi.app.printer.data.models.OrderStatus;
+import ozi.app.printer.data.models.PrintOrder;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OrderServices {
+
+    OrderCreationResponse createOrder(OrderCreationRequest request);
+    OrderCreationResponse getOrderById(String id);
+    boolean clearAllOrders();
+    boolean deleteOrderById(String id);
+    List<Order> getAllOrders();
+    List<Order> getOrdersByUsername(String username);
+    List<Order> getOrdersByDate(LocalDateTime dateTime);
+    List<Order> getOrdersByStatus(OrderStatus status);
 }
--- src/main/java/ozi/app/printer/services/orderService/OrderServicesImpl.java
+++ src/main/java/ozi/app/printer/services/orderService/OrderServicesImpl.java
@@ -1,14 +1,58 @@
 package ozi.app.printer.services.orderService;
 
+import org.hibernate.criterion.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ozi.app.printer.data.dtos.requests.OrderCreationRequest;
 import ozi.app.printer.data.dtos.requests.UserCreationRequest;
+import ozi.app.printer.data.dtos.responses.OrderCreationResponse;
 import ozi.app.printer.data.dtos.responses.UserCreationResponse;
+import ozi.app.printer.data.models.OrderStatus;
 import ozi.app.printer.data.models.PrintUser;
 import ozi.app.printer.data.repositories.UserRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class OrderServicesImpl implements OrderServices {
+    @Override
+    public OrderCreationResponse createOrder(OrderCreationRequest request) {
+        return null;
+    }
+
+    @Override
+    public OrderCreationResponse getOrderById(String id) {
+        return null;
+    }
+
+    @Override
+    public boolean clearAllOrders() {
+        return false;
+    }
+
+    @Override
+    public boolean deleteOrderById(String id) {
+        return false;
+    }
+
+    @Override
+    public List<Order> getAllOrders() {
+        return null;
+    }
+
+    @Override
+    public List<Order> getOrdersByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public List<Order> getOrdersByDate(LocalDateTime dateTime) {
+        return null;
+    }
+
+    @Override
+    public List<Order> getOrdersByStatus(OrderStatus status) {
+        return null;
+    }
 }
