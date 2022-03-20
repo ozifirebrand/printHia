a48f6f77b5592bd23257fa304d2f0ca1c30067ea
Ozioma <oziomaokoroafor@gmail.com>
2022-03-20 19:46:01 +0100

    Add application

---
+++ src/main/java/ozi/app/printer/PrinterApplication.java
@@ -0,0 +1,13 @@
+package ozi.app.printer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrinterApplication.class, args);
+	}
+
+}
