adf2a025369bcdece50656af630740634a3463eb
Ozioma <oziomaokoroafor@gmail.com>
2022-03-22 21:14:35 +0100

    Add find by email

--- src/main/java/ozi/app/printer/data/repositories/UserRepository.java
+++ src/main/java/ozi/app/printer/data/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package ozi.app.printer.data.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ozi.app.printer.data.models.PrintUser;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<PrintUser, String> {
+    Optional<PrintUser> findPrintUserByEmail(String email);
 }
