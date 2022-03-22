3bb88df9ade47b94fd657e8b675f939c55d3a0b4
Ozioma <oziomaokoroafor@gmail.com>
2022-03-22 19:49:55 +0100

    Create mapper class

--- src/main/java/ozi/app/printer/mapper/Mapper.java
+++ src/main/java/ozi/app/printer/mapper/Mapper.java
@@ -4,10 +4,12 @@ import ozi.app.printer.data.dtos.requests.*;
 import ozi.app.printer.data.models.PrintUser;
 
 public class Mapper {
-    public PrintUser map (UserCreationRequest request){
+    public static PrintUser map (UserCreationRequest request){
         PrintUser user = new PrintUser();
-
-
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
         return user;
 
     }
