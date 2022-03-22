e100148548a127592f23d40d163cd35ae12736de
Ozioma <oziomaokoroafor@gmail.com>
2022-03-22 16:50:21 +0100

    Add email attribute

--- src/main/java/ozi/app/printer/data/dtos/responses/OrderCreationResponse.java
+++ src/main/java/ozi/app/printer/data/dtos/responses/OrderCreationResponse.java
@@ -1,8 +1,9 @@
 package ozi.app.printer.data.dtos.responses;
 
+import lombok.Data;
 import ozi.app.printer.data.models.OrderStatus;
 import java.time.LocalDateTime;
-
+@Data
 public class OrderCreationResponse {
 
     private String imageUrl;
