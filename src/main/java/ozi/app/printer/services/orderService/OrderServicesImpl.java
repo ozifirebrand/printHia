6ba0e8cd5f1941e4ce13c971761e87e5eaab06f6
Ozioma <oziomaokoroafor@gmail.com>
2022-03-23 17:33:02 +0100

    Implement get by id

--- src/main/java/ozi/app/printer/services/orderService/OrderServicesImpl.java
+++ src/main/java/ozi/app/printer/services/orderService/OrderServicesImpl.java
@@ -1,48 +1,55 @@
 package ozi.app.printer.services.orderService;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ozi.app.printer.data.dtos.requests.OrderCreationRequest;
+import ozi.app.printer.data.dtos.requests.UserCreationRequest;
 import ozi.app.printer.data.dtos.responses.OrderCreationResponse;
 import ozi.app.printer.data.models.OrderStatus;
 import ozi.app.printer.data.models.PrintOrder;
 import ozi.app.printer.data.repositories.OrderRepository;
+import ozi.app.printer.exceptions.BusinessLogic;
+import ozi.app.printer.mapper.Mapper;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class OrderServicesImpl implements OrderServices {
     @Autowired
     private OrderRepository orderRepository;
+
     @Override
-    public OrderCreationResponse createOrder(OrderCreationRequest request) {
-        PrintOrder order = new PrintOrder();
-        order.setOrderDate(request.getOrderDate());
-        order.setImageUrl(request.getImageUrl());
-        order.setSize(request.getSize());
-        order.setOrderStatus(OrderStatus.PENDING);
-        order.setQuantity(request.getQuantity());
+    public OrderCreationResponse createOrder(OrderCreationRequest request) throws BusinessLogic {
+        validate(request);
+
+        PrintOrder order = Mapper.map(request);
+
+        order.setOrdered(true);
+        order.setOrderStatus(OrderStatus.ORDERED);
         PrintOrder savedOrder = orderRepository.save(order);
-        savedOrder.setOrdered(true);
-        savedOrder.setOrderStatus(OrderStatus.ORDERED);
         savedOrder.setDeliveryDate(savedOrder.getOrderDate().plusDays(3) );
 
-        OrderCreationResponse response = new OrderCreationResponse();
-        response.setOrderDate(savedOrder.getOrderDate());
-        response.setOrderStatus(savedOrder.getOrderStatus());
-        response.setQuantity(savedOrder.getQuantity());
-        response.setSize(savedOrder.getSize());
-        response.setDeliveryDate(savedOrder.getDeliveryDate());
-        response.setImageUrl(savedOrder.getImageUrl());
+        return Mapper.map(savedOrder);
+    }
 
-        return response;
+    private void validate(OrderCreationRequest request) throws BusinessLogic {
+        boolean orderIsEmpty= request.getOrderDate() == null;
+        boolean imageUrlIsEmpty= request.getImageUrl() == null;
+        boolean sizeIsEmpty = request.getSize()==0;
+        boolean quantityIsEmpty = request.getQuantity() == 0;
+
+        if ( orderIsEmpty || imageUrlIsEmpty|| sizeIsEmpty|| quantityIsEmpty ){
+            throw new BusinessLogic("The given details are incomplete");
+        }
     }
 
     @Override
-    public OrderCreationResponse getOrderById(String id) {
-        return null;
+    public PrintOrder getOrderById(String id) {
+        return orderRepository.findById(id).orElse(null);
     }
 
     @Override
