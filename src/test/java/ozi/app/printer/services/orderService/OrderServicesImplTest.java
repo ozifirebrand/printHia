4a3a4387fc60f29b520bde0d24fd9da9e083a4bc
Ozioma <oziomaokoroafor@gmail.com>
2022-03-23 17:33:35 +0100

    Test get by id

--- src/test/java/ozi/app/printer/services/orderService/OrderServicesImplTest.java
+++ src/test/java/ozi/app/printer/services/orderService/OrderServicesImplTest.java
@@ -8,18 +8,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 import ozi.app.printer.data.dtos.requests.OrderCreationRequest;
 import ozi.app.printer.data.dtos.responses.OrderCreationResponse;
 import ozi.app.printer.data.models.OrderStatus;
+import ozi.app.printer.data.models.PrintOrder;
+import ozi.app.printer.exceptions.BusinessLogic;
+import ozi.app.printer.exceptions.OrderExceptions;
 
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @SpringBootTest
 class OrderServicesImplTest {
     @Autowired
     private OrderServices orderServices;
 
+    private OrderCreationRequest request;
     @BeforeEach
     void setUp() {
+        request= new OrderCreationRequest();
+        request.setOrderDate(LocalDateTime.now());
+        request.setSize(12.5);
+        request.setImageUrl("imageUrl");
+        request.setQuantity(1);
     }
 
     @AfterEach
@@ -27,13 +36,13 @@ class OrderServicesImplTest {
     }
 
     @Test
-    public void createOrder() {
-        OrderCreationRequest request = new OrderCreationRequest();
-        request.setOrderDate(LocalDateTime.now());
-        request.setSize(12.5);
-        request.setImageUrl("imageUrl");
-        request.setQuantity(1);
+    public void createOrder() throws BusinessLogic {
+        //given
+
+        //when
         OrderCreationResponse response =orderServices.createOrder(request);
+
+        //assert
         assertThat(response).isNotNull();
         assertThat(response.getOrderDate()).isEqualTo(request.getOrderDate());
         assertThat(response.getOrderStatus()).isEqualTo(OrderStatus.ORDERED);
@@ -43,9 +52,44 @@ class OrderServicesImplTest {
     }
 
     @Test
-    public void getOrderById() {
+    public void testIncompleteDetails_AbsentSizeAttribute_ThrowIncompleteDetailsException(){
+        //given
+        OrderCreationRequest request = new OrderCreationRequest();
+        request.setOrderDate(LocalDateTime.now());
+        request.setImageUrl("imageUrl2");
+        request.setQuantity(1);
+        //when
+        //assert
+
+        assertThatThrownBy(()->orderServices.createOrder(request)).isInstanceOf(BusinessLogic.class).hasMessage("The given details are incomplete");
+
     }
 
+    @Test
+    public void getOrderById() throws BusinessLogic {
+        //given...
+        OrderCreationResponse response =orderServices.createOrder(request);
+
+        //when
+        PrintOrder foundOrder = orderServices.getOrderById(response.getId());
+        //assert
+        assertThat(foundOrder.getImageUrl()).isEqualTo(response.getImageUrl());
+//        assertThat(foundOrder.getOrderDate()).isEqualTo(response.getOrderDate());
+        assertThat(foundOrder.getOrderStatus()).isEqualTo(OrderStatus.ORDERED);
+        assertThat(foundOrder.getSize()).isEqualTo(response.getSize());
+        assertThat(foundOrder.getDeliveryDate()).isEqualTo(response.getDeliveryDate());
+        assertThat(foundOrder.getQuantity()).isEqualTo(response.getQuantity());
+
+
+    }
+
+    @Test
+    public void test_ThrowUserDoesNotExistException_WhenUnknownId_FindById(){
+        //assert
+        assertThatThrownBy(()->orderServices.getOrderById("an_invalid_id"))
+                .isInstanceOf(OrderExceptions.class)
+                .hasMessage("This user with id an_invalid_id does not exist");
+    }
     @Test
     public void clearAllOrders() {
     }
