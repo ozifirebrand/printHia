98c2ae8ed77eff3415c6028772059e2df41b9a81
Ozioma <oziomaokoroafor@gmail.com>
2022-03-23 20:38:50 +0100

    Add find by order date

--- src/main/java/ozi/app/printer/data/repositories/OrderRepository.java
+++ src/main/java/ozi/app/printer/data/repositories/OrderRepository.java
@@ -3,5 +3,9 @@ package ozi.app.printer.data.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ozi.app.printer.data.models.PrintOrder;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<PrintOrder, String> {
+    List<PrintOrder> findPrintOrderByOrderDate(LocalDateTime orderDate);
 }
