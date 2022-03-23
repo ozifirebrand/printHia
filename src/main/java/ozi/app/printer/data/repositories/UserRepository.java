107d9f26bf8e693b0bdb4f1e1fa9275e3c00d3e6
Ozioma <oziomaokoroafor@gmail.com>
2022-03-23 05:52:20 +0100

    Add delete methods

--- src/main/java/ozi/app/printer/data/repositories/UserRepository.java
+++ src/main/java/ozi/app/printer/data/repositories/UserRepository.java
@@ -3,10 +3,15 @@ package ozi.app.printer.data.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ozi.app.printer.data.models.PrintUser;
 
+import java.util.Collection;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<PrintUser, String> {
     Optional<PrintUser> findPrintUserByEmail(String email);
 
     void deletePrintUserByEmail(String email);
+
+    void deletePrintUserByUsername(String  username);
+
+    Optional<PrintUser> findPrintUserByUsername(String username);
 }
