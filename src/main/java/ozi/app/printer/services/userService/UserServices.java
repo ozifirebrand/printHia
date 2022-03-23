7644afb878901a1bf7c437e45f69ed2910509bbf
Ozioma <oziomaokoroafor@gmail.com>
2022-03-23 12:38:26 +0100

    refactor user services

--- src/main/java/ozi/app/printer/services/userService/UserServices.java
+++ src/main/java/ozi/app/printer/services/userService/UserServices.java
@@ -1,13 +1,11 @@
 package ozi.app.printer.services.userService;
 
-import org.springframework.stereotype.Service;
 import ozi.app.printer.data.dtos.requests.UserCreationRequest;
 import ozi.app.printer.data.dtos.responses.UserCreationResponse;
 import ozi.app.printer.data.models.PrintUser;
 import ozi.app.printer.exceptions.BusinessLogic;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserServices {
     UserCreationResponse createUser(UserCreationRequest request) throws BusinessLogic;
