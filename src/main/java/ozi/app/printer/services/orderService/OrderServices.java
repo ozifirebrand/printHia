da3f8efdf35f92b80486aef00218d03bdabcc0b8
Ozioma <oziomaokoroafor@gmail.com>
2022-03-23 17:32:48 +0100

    Implement get by id

--- src/main/java/ozi/app/printer/services/orderService/OrderServices.java
+++ src/main/java/ozi/app/printer/services/orderService/OrderServices.java
@@ -4,14 +4,15 @@ import ozi.app.printer.data.dtos.requests.OrderCreationRequest;
 import ozi.app.printer.data.dtos.responses.OrderCreationResponse;
 import ozi.app.printer.data.models.OrderStatus;
 import ozi.app.printer.data.models.PrintOrder;
+import ozi.app.printer.exceptions.BusinessLogic;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OrderServices {
 
-    OrderCreationResponse createOrder(OrderCreationRequest request);
-    OrderCreationResponse getOrderById(String id);
+    OrderCreationResponse createOrder(OrderCreationRequest request) throws BusinessLogic;
+    PrintOrder getOrderById(String id);
     boolean clearAllOrders();
     boolean deleteOrderById(String id);
     List<PrintOrder> getAllOrders();
