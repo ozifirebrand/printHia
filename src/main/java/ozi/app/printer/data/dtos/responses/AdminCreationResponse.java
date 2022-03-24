21cf508b23776bdcbcc041de60bf5d5dcd929ea8
Ozioma <oziomaokoroafor@gmail.com>
2022-03-24 22:37:43 +0100

    Refactor admin response dto

--- src/main/java/ozi/app/printer/data/dtos/responses/AdminCreationResponse.java
+++ src/main/java/ozi/app/printer/data/dtos/responses/AdminCreationResponse.java
@@ -1,4 +1,16 @@
 package ozi.app.printer.data.dtos.responses;
 
+import lombok.Data;
+
+@Data
 public class AdminCreationResponse {
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String username;
 }
