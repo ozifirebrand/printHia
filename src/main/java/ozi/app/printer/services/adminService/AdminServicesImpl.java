b76cfe7605af296170e667cd95fb6830e07903af
Ozioma <oziomaokoroafor@gmail.com>
2022-03-24 22:59:37 +0100

    Implement null_detail exception

--- src/main/java/ozi/app/printer/services/adminService/AdminServicesImpl.java
+++ src/main/java/ozi/app/printer/services/adminService/AdminServicesImpl.java
@@ -6,20 +6,32 @@ import ozi.app.printer.data.dtos.requests.AdminCreationRequest;
 import ozi.app.printer.data.dtos.responses.AdminCreationResponse;
 import ozi.app.printer.data.models.PrintAdmin;
 import ozi.app.printer.data.repositories.AdminRepository;
+import ozi.app.printer.exceptions.AdminException;
 import ozi.app.printer.mapper.Mapper;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class AdminServicesImpl implements AdminServices {
     @Autowired
     private AdminRepository adminRepository;
     @Override
-    public AdminCreationResponse createAdmin(AdminCreationRequest request) {
+    public AdminCreationResponse createAdmin(AdminCreationRequest request) throws AdminException {
+        validate(request);
         PrintAdmin admin = Mapper.map(request);
         PrintAdmin savedAdmin = adminRepository.save(admin);
         return Mapper.map(savedAdmin);
+    }
+    private void validate(AdminCreationRequest request) throws AdminException {
+        boolean firstNameIsEmpty= request.getFirstName() == null;
+        boolean lastNameIsEmpty= request.getLastName() == null;
+        boolean emailIsEmpty= request.getEmail() == null;
+        boolean usernameIsEmpty= request.getUsername() == null;
+        boolean phoneNumberIsEmpty= request.getPhoneNumber() == null;
+        boolean passwordIsEmpty= request.getPassword() == null;
+
+        if ( firstNameIsEmpty||lastNameIsEmpty||emailIsEmpty|| usernameIsEmpty||phoneNumberIsEmpty||passwordIsEmpty)
+            throw new AdminException("These details are incomplete");
     }
 
     @Override
