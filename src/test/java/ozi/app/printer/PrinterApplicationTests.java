3942f8d5a752f86a37fd0986f2cda65a7ebe7f58
Ozioma <oziomaokoroafor@gmail.com>
2022-03-24 21:20:37 +0100

    Main class commit

---
+++ src/test/java/ozi/app/printer/PrinterApplicationTests.java
@@ -0,0 +1,13 @@
+package ozi.app.printer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrinterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
