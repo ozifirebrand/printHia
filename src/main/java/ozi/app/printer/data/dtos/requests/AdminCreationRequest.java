514e983e323ecfb0254ebcbfd231a150d0c9f9e2
Ozioma <oziomaokoroafor@gmail.com>
2022-03-24 22:38:17 +0100

    Refactor admin request dto

--- src/main/java/ozi/app/printer/data/dtos/requests/AdminCreationRequest.java
+++ src/main/java/ozi/app/printer/data/dtos/requests/AdminCreationRequest.java
@@ -1,4 +1,18 @@
 package ozi.app.printer.data.dtos.requests;
 
+import lombok.Data;
+
+@Data
 public class AdminCreationRequest {
+    private String firstName;
+
+    private String lastName;
+
+    private String phoneNumber;
+
+    private String email;
+
+    private String username;
+
+    private String password;
 }
