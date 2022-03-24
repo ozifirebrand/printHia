6a29711a389aea06ce018359a02bdbc0f2f42e6f
Ozioma <oziomaokoroafor@gmail.com>
2022-03-24 22:36:54 +0100

    Implement create admin

--- src/main/java/ozi/app/printer/services/adminService/AdminServicesImpl.java
+++ src/main/java/ozi/app/printer/services/adminService/AdminServicesImpl.java
@@ -1,17 +1,34 @@
 package ozi.app.printer.services.adminService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ozi.app.printer.data.dtos.requests.AdminCreationRequest;
 import ozi.app.printer.data.dtos.responses.AdminCreationResponse;
 import ozi.app.printer.data.models.PrintAdmin;
+import ozi.app.printer.data.repositories.AdminRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AdminServicesImpl implements AdminServices {
+    @Autowired
+    private AdminRepository adminRepository;
     @Override
     public AdminCreationResponse createAdmin(AdminCreationRequest request) {
-        return null;
+        AdminCreationResponse response = new AdminCreationResponse();
+        PrintAdmin admin = new PrintAdmin();
+        admin.setEmail(request.getEmail());
+        admin.setFirstName(request.getFirstName());
+        admin.setUsername(request.getUsername());
+        admin.setLastName(request.getLastName());
+        PrintAdmin savedAdmin = adminRepository.save(admin);
+        response.setId(savedAdmin.getId());
+        response.setUsername(savedAdmin.getUsername());
+        response.setEmail(savedAdmin.getEmail());
+        response.setFirstName(savedAdmin.getFirstName());
+        response.setLastName(savedAdmin.getLastName());
+        return response;
     }
 
     @Override
