0aa2736611f6612f6b48b5dbe2ff8e1140f63b63
Ozioma <oziomaokoroafor@gmail.com>
2022-03-24 21:44:19 +0100

    Create admin entity

---
+++ src/main/java/ozi/app/printer/data/models/PrintAdmin.java
@@ -0,0 +1,31 @@
+package ozi.app.printer.data.models;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+@Entity
+@Data
+public class PrintAdmin {
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(strategy="uuid", name="system-uuid")
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phoneNumber;
+
+    @Column(unique= true)
+    private String email;
+
+    private String username;
+
+    private String password;
+}
