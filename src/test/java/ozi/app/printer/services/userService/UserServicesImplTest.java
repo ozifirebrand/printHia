3422e51f91c76257e73c6ea29e552c1a0eae9ab7
Ozioma <oziomaokoroafor@gmail.com>
2022-03-28 19:38:34 +0100

    Optimise imports

--- src/test/java/ozi/app/printer/services/userService/UserServicesImplTest.java
+++ src/test/java/ozi/app/printer/services/userService/UserServicesImplTest.java
@@ -2,13 +2,20 @@ package ozi.app.printer.services.userService;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ozi.app.printer.data.dtos.requests.OrderCreationRequest;
 import ozi.app.printer.data.dtos.requests.UserCreationRequest;
+import ozi.app.printer.data.dtos.responses.OrderCreationResponse;
 import ozi.app.printer.data.dtos.responses.UserCreationResponse;
 import ozi.app.printer.data.models.PrintUser;
 import ozi.app.printer.data.models.Role;
 import ozi.app.printer.exceptions.BusinessLogicException;
+import ozi.app.printer.services.orderService.OrderServices;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -17,6 +24,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class UserServicesImplTest {
 
     @Autowired
+    @Mock
     private UserServices userServices;
 
     private UserCreationRequest request;
@@ -42,6 +50,7 @@ class UserServicesImplTest {
         assertThat(response.getEmail()).isEqualTo("firstname@mail.com");
         assertThat(response.getFirstName()).isEqualTo("firstname");
         assertThat(response.getLastName()).isEqualTo("lastname");
+        assertThat(response.getOrders().size()).isEqualTo(0);
         userServices.deleteAllUsers();
     }
 
