2a4ab275fda450674278c91c27971acd54c8a759
Ozioma <oziomaokoroafor@gmail.com>
2022-03-28 19:10:42 +0100

    Add find order by user id

--- src/main/java/ozi/app/printer/data/repositories/OrderRepository.java
+++ src/main/java/ozi/app/printer/data/repositories/OrderRepository.java
@@ -3,14 +3,15 @@ package ozi.app.printer.data.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ozi.app.printer.data.models.OrderStatus;
 import ozi.app.printer.data.models.PrintOrder;
+import ozi.app.printer.data.models.PrintUser;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<PrintOrder, String> {
-    List<PrintOrder> getByOrderDate(LocalDateTime date);
+    List<PrintOrder> findByOrderDate(LocalDateTime date);
 
     List<PrintOrder> findByOrderStatus(OrderStatus status);
 
-    List<PrintOrder> findByUsername(String username);
+    List<PrintOrder> findPrintOrderByUserId(String userId);
 }
