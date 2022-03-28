dcc8fdf011709bee1a3b07b8376c386ca31f75e9
Ozioma <oziomaokoroafor@gmail.com>
2022-03-28 19:15:26 +0100

    Remove username set/get

--- src/test/java/ozi/app/printer/data/repositories/AdminRepositoryTest.java
+++ src/test/java/ozi/app/printer/data/repositories/AdminRepositoryTest.java
@@ -10,7 +10,6 @@ import ozi.app.printer.data.models.PrintAdmin;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 class AdminRepositoryTest {
 
@@ -31,7 +30,6 @@ class AdminRepositoryTest {
         PrintAdmin admin = new PrintAdmin();
         admin.setPassword("mupassword");
         admin.setEmail("myemail.com");
-        admin.setUsername("myusername");
         admin.setLastName("mylastname");
         admin.setPhoneNumber("9849300");
         admin.setFirstName("myfirstname");
@@ -41,7 +39,6 @@ class AdminRepositoryTest {
         PrintAdmin admin1 = new PrintAdmin();
         admin1.setPassword("mpassword");
         admin1.setEmail("memail.com");
-        admin1.setUsername("musername");
         admin1.setLastName("mlastname");
         admin1.setPhoneNumber("98949300");
         admin1.setFirstName("mfirstname");
@@ -51,14 +48,12 @@ class AdminRepositoryTest {
 
         assertThat(savedAdmin.getPassword()).isEqualTo(admin.getPassword());
         assertThat(savedAdmin.getEmail()).isEqualTo(admin.getEmail());
-        assertThat(savedAdmin.getUsername()).isEqualTo(admin.getUsername());
         assertThat(savedAdmin.getLastName()).isEqualTo(admin.getLastName());
         assertThat(savedAdmin.getPhoneNumber()).isEqualTo(admin.getPhoneNumber());
         assertThat(savedAdmin.getFirstName()).isEqualTo(admin.getFirstName());
 
         assertThat(savedAdmin1.getPassword()).isEqualTo(admin1.getPassword());
         assertThat(savedAdmin1.getEmail()).isEqualTo(admin1.getEmail());
-        assertThat(savedAdmin1.getUsername()).isEqualTo(admin1.getUsername());
         assertThat(savedAdmin1.getLastName()).isEqualTo(admin1.getLastName());
         assertThat(savedAdmin1.getPhoneNumber()).isEqualTo(admin1.getPhoneNumber());
         assertThat(savedAdmin1.getFirstName()).isEqualTo(admin1.getFirstName());
@@ -72,14 +67,12 @@ class AdminRepositoryTest {
         assertThat(admins.get(0).getPassword()).isEqualTo(savedAdmin.getPassword());
         assertThat(admins.get(0).getLastName()).isEqualTo(savedAdmin.getLastName());
         assertThat(admins.get(0).getFirstName()).isEqualTo(savedAdmin.getFirstName());
-        assertThat(admins.get(0).getUsername()).isEqualTo(savedAdmin.getUsername());
         assertThat(admins.get(0).getEmail()).isEqualTo(savedAdmin.getEmail());
 
         assertThat(admins.get(1).getId()).isEqualTo(savedAdmin1.getId());
         assertThat(admins.get(1).getPassword()).isEqualTo(savedAdmin1.getPassword());
         assertThat(admins.get(1).getLastName()).isEqualTo(savedAdmin1.getLastName());
         assertThat(admins.get(1).getFirstName()).isEqualTo(savedAdmin1.getFirstName());
-        assertThat(admins.get(1).getUsername()).isEqualTo(savedAdmin1.getUsername());
         assertThat(admins.get(1).getEmail()).isEqualTo(savedAdmin1.getEmail());
     }
     @Test
@@ -90,12 +83,10 @@ class AdminRepositoryTest {
         admin.setEmail("myemail.com");
         admin.setFirstName("firstname");
         admin.setLastName("lastName");
-        admin.setUsername("username");
         admin.setPassword("password");
         PrintAdmin savedAdmin = repository.save(admin);
 
         assertThat(savedAdmin.getId()).isNotNull();
-        assertThat(savedAdmin.getUsername()).isEqualTo(admin.getUsername());
         assertThat(savedAdmin.getEmail()).isEqualTo(admin.getEmail());
         assertThat(savedAdmin.getPhoneNumber()).isEqualTo(admin.getPhoneNumber());
 
@@ -104,7 +95,6 @@ class AdminRepositoryTest {
 
         //assert
         assertThat(foundAdmin.getId()).isNotNull();
-        assertThat(foundAdmin.getUsername()).isEqualTo(admin.getUsername());
         assertThat(foundAdmin.getEmail()).isEqualTo(admin.getEmail());
         assertThat(foundAdmin.getPhoneNumber()).isEqualTo(admin.getPhoneNumber());
     }
