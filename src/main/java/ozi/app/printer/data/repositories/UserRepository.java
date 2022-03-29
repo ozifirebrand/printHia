53f772291ff8676820c92bc056d2f25cf34b8027
Ozioma <oziomaokoroafor@gmail.com>
2022-03-29 12:14:48 +0100

    Delete find by username

--- src/main/java/ozi/app/printer/data/repositories/UserRepository.java
+++ src/main/java/ozi/app/printer/data/repositories/UserRepository.java
@@ -3,11 +3,8 @@ package ozi.app.printer.data.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ozi.app.printer.data.models.PrintUser;
 
-import java.util.Collection;
 import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<PrintUser, String> {
     Optional<PrintUser> findPrintUserByEmail(String email);
 
-    Optional<PrintUser> findPrintUserByUsername(String username);
 }
