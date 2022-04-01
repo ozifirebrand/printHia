1b0546e791745ecd5b41a61a1db4ee042f7a8cd9
Ozioma <oziomaokoroafor@gmail.com>
2022-04-01 16:02:01 +0100

    Commit all changes on vuejs

--- src/main/java/ozi/app/printer/web/controllers/HomeController.java
+++ src/main/java/ozi/app/printer/web/controllers/HomeController.java
@@ -3,18 +3,18 @@ package ozi.app.printer.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
 @RequestMapping("/home")
-
+@Controller
 public class HomeController {
 
     @GetMapping("/welcome")
     public ModelAndView welcome(){
 
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index.html");
+        modelAndView.setViewName("index");
         return modelAndView;
     }
 }
