ee6dca8e0bf8bde58f3ee9bef34133b7407ee04f
Ozioma <oziomaokoroafor@gmail.com>
2022-04-21 19:28:44 +0100

    Add object mapper bean

--- src/main/java/ozi/app/printer/config/ApplicationConfiguration.java
+++ src/main/java/ozi/app/printer/config/ApplicationConfiguration.java
@@ -1,5 +1,9 @@
 package ozi.app.printer.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,5 +12,16 @@ public class ApplicationConfiguration implements WebMvcConfigurer {
      public void addResourceHandlers(final ResourceHandlerRegistry registry) {
          registry.addResourceHandler("/resources/").addResourceLocations("classpath:/resources/static/");
 
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        return objectMapper;
     }
 }
